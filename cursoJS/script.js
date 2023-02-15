a36bb95105f2cbd14205a207f88d006c106cee93
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-14 21:38:58 -0300

    Operadores Aritmeticos

--- cursoJS/script.js
+++ cursoJS/script.js
@@ -1,5 +1,15 @@
-const mostraNome = function(nome) {
-    return 'meu nome é ' + nome
-}
+console.log('Adição (+):', 2 + 2);
+console.log('Subtração (-):', 8 - 3);
+console.log('Divisão (/):', 12 / 2);
+console.log('Multiplicação (*):', 5 * 8);
+console.log('Módulo (%):', 10 % 2);
 
-console.log(mostraNome('João'));
+var valor = 10;
+console.log('Incremento (++):', ++valor);
+console.log(valor);
+
+valor = 10
+console.log('Decremento (---):', valor--);
+console.log(valor);
+
+console.log('Exponenciação (**):', 2 ** 3);
