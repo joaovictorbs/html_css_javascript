9a15b7dc4869118f2ec6e0fbab9145690660a1f1
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-14 22:00:03 -0300

    Igual e estritamente igual

--- cursoJS/script.js
+++ cursoJS/script.js
@@ -1,5 +1,15 @@
-console.log(10 > 12);
-console.log(10 < 12);
-console.log(12 >= 9);
-console.log(12 <= 9);
-console.log(10 != 10);
+var num = 18;
+var idade = '18';
+
+console.log(num == idade);
+console.log(num === idade);
+
+if(idade === num) {
+    console.log('É estritamente igual!');
+}
+else if(idade == num) {
+    console.log('É igual!');
+}
+else {
+    console.log('Não é igual!');
+}
