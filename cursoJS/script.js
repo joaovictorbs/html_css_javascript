d1f54e7907ee3f63b31f902e2cf7561ea4b8fca6
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-14 21:09:52 -0300

    Escopo no JS

--- cursoJS/script.js
+++ cursoJS/script.js
@@ -1,3 +1,17 @@
-var nome = 'joao';
-let idade = 20;
-const dataDeNascimento = '14/01/2023';
+// var, let e const
+//escopo global, escopo global
+
+let animal = 'cachorro';
+const animalconst = 'teste1'
+
+if(true){
+    let animal = 'gato'
+    const animalconst = 'teste2'
+    console.log(animal)
+    console.log(animalconst)
+
+}
+animalconst = 'teste3'
+
+console.log(animal)
+console.log(animalconst)
