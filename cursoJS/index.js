a2171528c56aabb4840e7cd659d2a583a98aa5fa
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-16 22:51:26 -0300

    getAttribute(), setAttribute() & removeAttribute()

--- cursoJS/index.js
+++ cursoJS/index.js
@@ -1,14 +1,7 @@
-const container = document.getElementById('container'); //captura elemento
-const logoJS = document.createElement('img');   //cria elemento imagem
-const title = document.createElement('h1');  
+const logo = document.getElementById('logo');
+const attribute = logo.getAttribute('alt') //captura o atributo alt
 
-logoJS.src = './js.png';    //associa imagem ao elemento criado
-logoJS.width = '200';
+console.log(attribute)  //mostra o texto dentro do atributo alt
 
-title.innerHTML = 'Logo do JavaScript'; //Adiciona titulo ao elemento h1
-
-container.appendChild(title);
-container.appendChild(logoJS);  //adiciona o elemento logoJS dentro da div
-                                //container no HTML.
-
-//container.removeChild(title);   //remove elemento h1
+logo.setAttribute('width', '200');  //cria atributo com valor
+logo.removeAttribute('width');  //remove atributo
