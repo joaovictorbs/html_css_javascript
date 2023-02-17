ed987ffaf546e2798e4e6e8b83cf1cb4c5ff4de6
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-16 22:35:54 -0300

    AppendChild() & RemoveChild()

--- cursoJS/index.js
+++ cursoJS/index.js
@@ -1,4 +1,14 @@
-const div = document.createElement('div');  //cria elemento
-div.innerHTML = 'Olá Mundo!';   // trabalha em cima do elemento
+const container = document.getElementById('container'); //captura elemento
+const logoJS = document.createElement('img');   //cria elemento imagem
+const title = document.createElement('h1');  
 
-console.log(div);
+logoJS.src = './js.png';    //associa imagem ao elemento criado
+logoJS.width = '200';
+
+title.innerHTML = 'Logo do JavaScript'; //Adiciona titulo ao elemento h1
+
+container.appendChild(title);
+container.appendChild(logoJS);  //adiciona o elemento logoJS dentro da div
+                                //container no HTML.
+
+//container.removeChild(title);   //remove elemento h1
