ec3864d59d3050da61a9617f8ddc5180313436b5
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-19 19:13:37 -0300

    Onsubmit

--- cursoJS/index.js
+++ cursoJS/index.js
@@ -1,3 +1,10 @@
 function enviar(){
-    alert('Alo!')
+    const idade = document.getElementById('age')    //captura elemento input
+
+    if (idade.value < 18) {         //pega somente o valor do input
+        alert('Menor de idade!');
+    }
+    else {
+        alert('Maior de idade!')
+    }
 }
