46bceeb11ceec7ebdb8669e67adb7c6dfcafeb97
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-19 18:39:03 -0300

    OnClick

--- cursoJS/index.js
+++ cursoJS/index.js
@@ -1,15 +1,9 @@
-const main = document.getElementById('container');
-const div = document.getElementById('lista-container');
-const ul = document.getElementById('lista');
+function saudacoes(){
+    alert('Olá :)');
+}
 
-console.log(main.childNodes);   //pega filhos diretos do nó main
-console.log(div.childNodes);
-console.log(ul.childNodes);
-console.log(ul.childNodes[1]);  //pega elemento de indice 1 no elemento ul
+function mostraCaixa(){
+    const caixa = document.getElementById('caixa');
 
-ul.childNodes[1].innerHTML = 'Item 0';  //altera elemento de indice 1
-
-console.log(document.documentElement.firstChild); //pega primeiro nó do documento HTML
-
-console.log(ul.parentNode)  // pega elemento pai de um nó
-console.log(ul.parentNode.parentNode)  // pega elemento pai de um nó pai
+    caixa.style.display = 'block';
+}
