d685344d2c52db1973d6a64909356f0dbad38ecc
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-22 22:53:02 -0300

    Iniciando o JavaScript do projeto

---
+++ guessNumber/js/index.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit());
+
+const statusTitle = document.getElementById('status');
+const attempt = document.getElementById('attempt');
+const inputValue = document.getElementById('Kick');
+const result = document.getElementById('result');
+const btnRestart = document.getElementById('btn-restart');
+
+function handleSubmit(e){   //recebe evento
+    alert('Olá');
+}        
