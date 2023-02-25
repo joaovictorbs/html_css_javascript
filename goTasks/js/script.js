4229a04764bf33a2ae3b02b4bef6a0b90041fb62
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-02-25 16:06:38 -0300

    Salvando tarefa no LocalStorage

--- goTasks/js/script.js
+++ goTasks/js/script.js
@@ -7,17 +7,33 @@ const loadingMessage = document.getElementById('loading-message');
 const countTasks = document.getElementById('count-tasks');
 const btnCreateTask = document.getElementById('btn-create-task');
 
-// FUNCOES MODAL========
-function openModal(){       //abre tela para informar nova tarefa
-    modal.style.display = 'flex';   
+// MODAL========
+btnCreateTask.addEventListener('click', createTask);
+
+
+function createTask(e) {             //recebe evento = estado atual de tudo
+    e.preventDefault(e)             //evita que aconteca o funcionamento padrao, tela não recarrega
+
+    if(!inputDescription.value || !inputDate.value) {
+        alert('Preencha todos os campos');
+        return
+    }
+
+    const newTask = {                           //objeto
+        description: inputDescription.value,
+        date: new Date(inputDate.value).toLocaleDateString(),  //formato de data da regiao do navegador
+        id: Math.floor(Math.random() * 10000)                   //numero aleatorio entre 0 e 10000, e arredondado   
+    }
+
+    localStorage.setItem('@GoTask', JSON.stringify( [ newTask ]));
 }
 
-function closeModal(){       //fecha tela para informar nova tarefa
-    modal.style.display = 'none';   
+function toggleModal() {                        //abre e fecha para informar nova tarefa
+    modal.classList.toggle('modal-visible');   //funcao verifica se elemento possui classe, se nao tem adiciona, se tem remove
     clearFields();
 }
 
-function clearFields() {    //zera os valores do formulario quando fecha
+function clearFields() {                //zera os valores do formulario quando fecha
     inputDescription.value = ''; 
     inputDate.value = ''
 }
